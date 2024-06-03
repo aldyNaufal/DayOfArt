af8c74f1c370b2b2356e8d162e23af17cef44ff0
Aldy Naufal <aldy.naufal02@gmail.com>
2024-06-03 14:08:33 +0700

    button update

--- resources/views/editProfile.blade.php
+++ resources/views/editProfile.blade.php
@@ -30,7 +30,7 @@
                     <label for="phone" class="block text-sm font-medium text-gray-700">Phone number</label>
                     <input type="text" id="phone" name="phone" class="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" placeholder="Type your phone number here">
                 </div>
-                <input type="submit" class="w-full bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded" >Save data</input>
+                <input type="submit" class="w-full bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded" name="Save data" value="Save data" ></input>
             </form>
         </div>
     </div>
