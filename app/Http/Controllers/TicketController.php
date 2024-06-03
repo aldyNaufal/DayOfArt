e61f51fe0ef4e768bde9090eb972f36a5fa01780
jonatyoung <jonathanyoung979@gmail.com>
2024-06-03 13:16:54 +0700

    profile my ticket

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ticket;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TicketController extends Controller
 {
     //
+
+    public function myTicket(){
+        $userId = Auth::user()->id;
+        $query = Ticket::query();
+        $query->where('idUser',$userId);
+
+        $tickets = $query->get();
+        return view('profileMyTicket',['tickets' => $tickets]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TicketController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
@@ -99,3 +100,4 @@ Route::get('/editProfile', [ProfileController::class, 'profileEdit'])->name('pro
 Route::put('/profile/{user}/update', [ProfileController::class, 'profileUpdate'])->name('profile.update');
 Route::get('/profilePersonalData', [ProfileController::class, 'profilePersonalData'])->name('profile.personal');
 
+Route::get('/profileMyTicket', [TicketController::class, 'myTicket'])->name('profile.ticket');
