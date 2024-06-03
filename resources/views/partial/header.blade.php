a4bf3ee9bbaffe41eb84437abc56af37f65ccdf8
unknown <nada.firdaus@gmail.com>
2024-06-03 10:34:26 +0700

    benerin search sama activities apply

--- resources/views/partial/header.blade.php
+++ resources/views/partial/header.blade.php
@@ -17,10 +17,11 @@
         <a href="#" class="hover:text-[#872964] font-bold 2xl:text-xl sm:text-xs md:text-sm lg:text-md xl:text-lg">FaQ</a>
         <a href="/profile" class="hover:text-[#872964] font-bold 2xl:text-xl sm:text-xs md:text-sm lg:text-md xl:text-lg">Profile</a>
     </div>
-    <div class="relative w-1/4 xl:w-1/3 sm:ml-2 md:ml-10 lg:ml-[220px] xl:ml-[300px] 2xl:ml-[280px]">
-        <input type="text" class="w-full sm:px-2 sm:py-0 xl:py-2 lg:px-4 border border-gray-300 sm:rounded-2xl xl:rounded-6xl outline-none" placeholder="Search for...">
+    
+    <form action="/search" method="GET" class="relative w-1/4 xl:w-1/3 sm:ml-2 md:ml-10 lg:ml-[220px] xl:ml-[300px] 2xl:ml-[280px]">
+        <input type="text" name="query" class="w-full sm:px-2 sm:py-0 xl:py-2 lg:px-4 border border-gray-300 sm:rounded-2xl xl:rounded-6xl outline-none" placeholder="Search for...">
         <img src="{{ asset('assets/icons/search-events.png') }}" alt="Search Icon" class="absolute right-5 top-1/2 transform -translate-y-1/2 sm:w-3 sm:h-3 xl:w-5 xl:h-5">
-    </div>
+    </form>
     
     @auth
     <div class="flex flex-row justify-center items-center md:mr-5 lg:mr-15 xl:mr-[50px] 2xl:ml-[20px]">
