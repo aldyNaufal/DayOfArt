8fab38f47546a638617b2fda7b9ae60cce65cf14
Aldy Naufal <aldy.naufal02@gmail.com>
2024-06-03 12:27:10 +0700

    header update

--- resources/views/partial/header.blade.php
+++ resources/views/partial/header.blade.php
@@ -3,18 +3,18 @@
     <div class="flex flex-row 2xl:gap-8 xl:gap-6 lg:gap-6 md:gap-6 sm:gap-5 text-[#fcecf6] font-light items-center">
         <img src="{{ asset('assets/icons/logo DoA transparent 2.png') }}" alt="Logo doa" class="sm:w-12 sm:ml-4 md:w-15 lg:w-18 xl:w-20 2xl:w-22 ">
         <a href="/" class="hover:text-[#872964] font-bold 2xl:text-xl sm:text-xs md:text-sm lg:text-md xl:text-lg">Home</a>
-        <a href="static/about" class="hover:text-[#872964] font-bold 2xl:text-xl sm:text-xs md:text-sm lg:text-md xl:text-lg">About</a>
+        <a href="/about" class="hover:text-[#872964] font-bold 2xl:text-xl sm:text-xs md:text-sm lg:text-md xl:text-lg">About</a>
         <div class="relative group">
             <p class="hover:text-[#872964] font-bold 2xl:text-xl sm:text-xs md:text-sm lg:text-md xl:text-lg cursor-pointer">Activities</p>
             <ul class="absolute hidden group-hover:block bg-[#E145A6] text-[#fcecf6] shadow-md rounded-md z-10">
-                <li class="px-4 py-2 hover:bg-[#fbe3f2] hover:text-[#972964]"><a href="/">All Activities</a></li>
+                <li class="px-4 py-2 hover:bg-[#fbe3f2] hover:text-[#972964]"><a href="/activities">All Activities</a></li>
                 <li class="px-4 py-2 hover:bg-[#fbe3f2] hover:text-[#972964]"><a href="#">Pottery</a></li>
                 <li class="px-4 py-2 hover:bg-[#fbe3f2] hover:text-[#972964]"><a href="#">Cooking Class</a></li>
                 <li class="px-4 py-2 hover:bg-[#fbe3f2] hover:text-[#972964]"><a href="#">Flower Bucket</a></li>
                 <li class="px-4 py-2 hover:bg-[#fbe3f2] hover:text-[#972964]"><a href="#">Chunky Bag</a></li>
             </ul>
         </div>
-        <a href="faq" class="hover:text-[#872964] font-bold 2xl:text-xl sm:text-xs md:text-sm lg:text-md xl:text-lg">FaQ</a>
+        <a href="/faq" class="hover:text-[#872964] font-bold 2xl:text-xl sm:text-xs md:text-sm lg:text-md xl:text-lg">FaQ</a>
         <a href="/profile" class="hover:text-[#872964] font-bold 2xl:text-xl sm:text-xs md:text-sm lg:text-md xl:text-lg">Profile</a>
     </div>
     
