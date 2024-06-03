54a5b6001e0b4e1e842cf74ae4a2508744c34caa
Aldy Naufal <aldy.naufal02@gmail.com>
2024-06-03 11:47:41 +0700

    cape

--- resources/views/activities.blade.php
+++ resources/views/activities.blade.php
@@ -12,34 +12,35 @@
                     <div class="form-group mb-4">
                         <label for="category" class="block">Activities</label>
                         <select name="category" id="category" class="bg-white border border-gray-300 rounded-md py-2 px-4 shadow-sm flex items-center space-x-2" >
-                            <option value="1">bag</option>
-                            <option value="2">pottery</option>
-                            <option value="3">cooking</option>
-                            <option value="4">flower</option>
+                            <option value="bag">bag</option>
+                            <option value="pottery">pottery</option>
+                            <option value="cooking">cooking</option>
+                            <option value="flower">flower</option>
                         </select>
                     </div>
+                    <button type="submit" class="bg-pink-500 text-white py-2 px-4 rounded-md shadow-sm hover:bg-pink-600 transition-colors duration-300">Apply</button>
                 </form>
             </div>
         </div>
 
-    <div class="justify-start items-center">
-        <div class="px-10 py-20 relative overflow-hidden sm:px-5 xl:mt-5">
-            <div class="grid grid-cols-4 gap-10 sm:mt-10 sm:flex sm:flex-col sm:gap-5 lg:flex-row ml-20">
+        <div class="justify-start items-center">
+        <div class="px-10 py-20 relative overflow-hidden  sm:px-5 xl:mt-5">
+            <div class="grid grid-cols-4 gap-10 sm:mt-10 m:flex sm:flex-col sm:gap-5 lg:flex-row ml-20">
                 @foreach($events as $e)
-                <a href="{{ route('detail') }}">
+                <a href="{{ route('detail')}}">
                     <div class="px-5 py-2 pb-10 shadow-lg rounded-md bg-white h-96 cursor-pointer transform transition-transform duration-300 hover:scale-105">
-                        <img src="{{ asset($e->image) }}" alt="{{ $e->name }}" class="rounded-md">
+                        <img src="{{ asset( $e->image ) }}" alt="{{ $e->name }}" class="rounded-md">
                         <div class="flex flex-row justify-between mt-5 items-center">
-                            <h1 class="text-xl font-bold">{{ $e->name }}</h1>
+                            <h1 class="text-xl font-bold">{{$e->name}}</h1>
                             <p class="text-sm text-gray-400">{{ $e->date }}</p>
                         </div>
                         <p class="text-sm text-gray-400 mt-2 mb-3">{{ $e->city }}</p>
                     </div>
                 </a>
-                @endforeach
+                @endforeach     
             </div>
         </div>
-    </div>
-</main>
+    </main>
+</body>
 
 @endsection
